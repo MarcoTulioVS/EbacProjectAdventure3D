2e1a9324dadccae03e5c4d0bbf83935d1a1c384a
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2024-07-24 11:07:42 -0300

    Adding state machine

---
+++ EbacAdventure3D/Assets/Scripts/Ebac/StateMachine/StateMachine.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    
+    public enum States
+    {
+        NONE
+    }
+
+    public Dictionary<States, StateBase> dictionaryState;
+
+    private StateBase _currentState;
+    public float timeToStartGame = 1f;
+
+    private void Awake()
+    {
+        dictionaryState = new Dictionary<States, StateBase>();
+        dictionaryState.Add(States.NONE, new StateBase());
+
+        SwitchState(States.NONE);
+
+        Invoke(nameof(StartGame), timeToStartGame);
+    }
+
+    private void StartGame()
+    {
+        SwitchState(States.NONE);
+    }    
+    private void SwitchState(States state)
+    {
+        if (_currentState != null)
+        {
+            _currentState.OnStateExit();
+        }
+        _currentState = dictionaryState[state];
+        _currentState.OnStateEnter();
+    }
+
+    private void Update()
+    {
+        if(_currentState != null)
+        {
+            _currentState.OnStateStay();
+        }
+    }
+}
