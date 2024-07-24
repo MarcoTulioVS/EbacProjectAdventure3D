19e1656bfc806dbb4d2485d090a3cc65fa21b30f
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2024-07-24 12:41:34 -0300

    Updating state machine
    adding fsm(finite state machine)

--- EbacAdventure3D/Assets/Scripts/Ebac/StateMachine/StateMachine.cs
+++ EbacAdventure3D/Assets/Scripts/Ebac/StateMachine/StateMachine.cs
@@ -1,35 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
-public class StateMachine : MonoBehaviour
+public class Test
 {
-    
-    public enum States
+    public enum Test2
     {
         NONE
     }
 
-    public Dictionary<States, StateBase> dictionaryState;
+    public void Aa()
+    {
+        StateMachine<Test2>stateMachine = new StateMachine<Test2>();
+        stateMachine.RegisterStates(Test.Test2.NONE, new StateBase());
+    }
+}
+public class StateMachine<T> where T : System.Enum
+{
+    
+    public Dictionary<T, StateBase> dictionaryState;
 
     private StateBase _currentState;
     public float timeToStartGame = 1f;
 
-    private void Awake()
+    public StateBase CurrentState
     {
-        dictionaryState = new Dictionary<States, StateBase>();
-        dictionaryState.Add(States.NONE, new StateBase());
-
-        SwitchState(States.NONE);
-
-        Invoke(nameof(StartGame), timeToStartGame);
+        get { return _currentState; }
     }
 
-    private void StartGame()
+    public void Init()
     {
-        SwitchState(States.NONE);
-    }    
-    private void SwitchState(States state)
+        dictionaryState = new Dictionary<T, StateBase>();
+    }
+
+    public void RegisterStates(T typeEnum,StateBase state)
+    {
+        dictionaryState.Add(typeEnum,state);
+       
+    }
+
+
+    public void SwitchState(T state)
     {
         if (_currentState != null)
         {
@@ -39,7 +51,7 @@ public class StateMachine : MonoBehaviour
         _currentState.OnStateEnter();
     }
 
-    private void Update()
+    public void Update()
     {
         if(_currentState != null)
         {
