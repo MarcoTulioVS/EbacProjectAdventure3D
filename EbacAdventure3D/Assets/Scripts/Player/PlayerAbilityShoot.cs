363168b6ee999a2d175105e7f401ba4167a5172f
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2024-09-27 16:52:40 -0300

    Adding shoot action

--- EbacAdventure3D/Assets/Scripts/Player/PlayerAbilityShoot.cs
+++ EbacAdventure3D/Assets/Scripts/Player/PlayerAbilityShoot.cs
@@ -1,8 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerAbilityShoot : PlayerAbilityBase
 {
-    
+    public InputAction shootAction;
+
+
+    protected override void Init()
+    {
+        base.Init();
+        shootAction.performed += ctx => Shoot();
+    }
+
+    private void Shoot()
+    {
+        Debug.Log("Shoot");
+    }
+
+    private void OnEnable()
+    {
+        shootAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        shootAction.Disable();
+    }
 }
