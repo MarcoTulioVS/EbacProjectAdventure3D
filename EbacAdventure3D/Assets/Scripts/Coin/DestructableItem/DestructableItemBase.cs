389dd550150138ae91bcf0e655342a9336039251
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2025-06-23 14:24:02 -0300

    Adding drop of coins and tree destruction

--- EbacAdventure3D/Assets/Scripts/Coin/DestructableItem/DestructableItemBase.cs
+++ EbacAdventure3D/Assets/Scripts/Coin/DestructableItem/DestructableItemBase.cs
@@ -9,6 +9,11 @@ public class DestructableItemBase : MonoBehaviour
     public float shakeDuration = 0.1f;
     public int shakeForce = 1;
 
+    public int dropCoinAmount;
+    public GameObject coinPrefab;
+
+    public Transform dropPosition;
+
     private void OnValidate()
     {
         if (healthBase == null)
@@ -26,5 +31,31 @@ public class DestructableItemBase : MonoBehaviour
     private void OnDamage(HealthBase h)
     {
         transform.DOShakeScale(shakeDuration, Vector3.up, shakeForce);
+        //DropCoins();
+        DropGroupOfCoins();
+    }
+
+    [NaughtyAttributes.Button]
+    private void DropCoins()
+    {
+        var i = Instantiate(coinPrefab);
+        i.transform.position = dropPosition.position;
+        i.transform.DOScale(0,1f).SetEase(Ease.OutBack).From();
+    }
+
+    [NaughtyAttributes.Button]
+    private void DropGroupOfCoins()
+    {
+        StartCoroutine("DropGroupOfCoinsCoroutine");
+    }
+
+    IEnumerator DropGroupOfCoinsCoroutine()
+    {
+        for (int i = 0; i < dropCoinAmount; i++)
+        {
+            DropCoins();
+            yield return new WaitForSeconds(0.1f);
+        }
+
     }
 }
