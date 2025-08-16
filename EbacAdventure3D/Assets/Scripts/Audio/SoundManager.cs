efa3151473bceffa008044b033fce21495bec57e
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2025-08-16 08:50:19 -0300

    Adding sound setup

--- EbacAdventure3D/Assets/Scripts/Audio/SoundManager.cs
+++ EbacAdventure3D/Assets/Scripts/Audio/SoundManager.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class SoundManager : MonoBehaviour
+using Ebac.Core.Singleton;
+public class SoundManager : Singleton<SoundManager>
 {
     public List<MusicSetup> musicSetups;
     public List<SFXSetup> sfxSetups;
 
+    public AudioSource musicSource;
 
+    public void PlayMusicByType(MusicType musicType)
+    {
+        var music  = GetMusicByType(musicType);
+        musicSource.clip = music.audioClip;
+        musicSource.Play();
+    }
+
+    public MusicSetup GetMusicByType(MusicType musicType)
+    {
+        return musicSetups.Find(i => i.type == musicType);
+    }
+    public SFXSetup GetSFXByType(SFXType sfxType)
+    {
+        return sfxSetups.Find(i => i.type == sfxType);
+    }
 }
 
 [System.Serializable]
